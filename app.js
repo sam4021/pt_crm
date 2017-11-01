1fc1cbfcd4beab531635d206ed9332ce56d94fdf
Samson Wandah <samsonwandah@gmail.com>
2017-11-01 11:05:36 +0300

    Update app.js

--- app.js
+++ app.js
@@ -25,9 +25,9 @@ db.on('error',function(err){
   console.log(err);
 });
 
-const port = 3000;
-
 const app = express();
+
+app.set('port', (process.env.PORT || 3000));
 
 //MORGAN Middleware
 app.use(morgan('dev'));
@@ -115,6 +115,6 @@ app.use('/products',products);
 
 
 //Start Server
-app.listen(port, function(){
-  console.log('Server started on port '+port+'....');
+app.listen(app.get('port'), function(){
+  console.log('Server started on port '+app.get('port')+'....');
 });
